18fe3dd4957e6b4a4959beacd37ace06211f1c58
Ocph23 <ocph23@gmail.com>
2021-11-23 17:48:41 +0900

    changeurl

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -14,55 +14,5 @@ export class HelperService {
 
   constructor() { }
 
-  // private messageShow(icon: any, mesage: string) {
-  //   Swal.fire({
-  //     position: 'top-end',
-  //     icon: icon,
-  //     title: mesage,
-  //     showConfirmButton: false,
-  //     timer: 3000
-  //   })
-  // }
-
-  // successMessage(mesage: string) {
-  //   this.messageShow('success', mesage)
-  // }
-  // errorMessage(mesage: string) {
-  //   this.messageShow('error', mesage)
-  // }
-  // infoMessage(mesage: string) {
-  //   this.messageShow('info', mesage)
-  // }
-  // warningMessage(mesage: string) {
-  //   this.messageShow('warning', mesage)
-  // }
-
-
-  // busy() {
-  //   Swal.fire({
-  //     customClass: {
-  //       popup : "busyid",
-  //     },
-  //     allowOutsideClick: false,
-  //   //  timerProgressBar: true,
-  //     didOpen: () => {
-  //       Swal.showLoading()
-  //     },
-  //     willClose: () => {
-        
-  //     }
-  //   }).then((result) => {
-      
-  //   })
-  // }
-
-
-  // busyStop(){
-  //   Swal.close();
-  // }
-
-
-
-
 
 }
