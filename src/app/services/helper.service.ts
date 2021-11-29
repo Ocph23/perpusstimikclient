e935b2e3c6a6a9935325420a5669ceecf76de519
Ocph23 <ocph23@gmail.com>
2021-11-29 14:28:27 +0900

    prod

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -7,7 +7,7 @@ export class HelperService {
 
   //apiUrl = "http://localhost/perpusstimik/webapp/public";
   apiUrl = "https://perpusstimikapi.stimiksepnop.ac.id";
-  //apiUrl = "http://localhost:8000";
+ // apiUrl = "http://localhost:8000";
 
   constructor() { }
 
