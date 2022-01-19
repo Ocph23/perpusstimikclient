2903ceb4279d1b8c3b5afb2b5a1454b4b67d1522
Ocph23 <ocph23@gmail.com>
2022-01-19 12:25:09 +0900

    change helper

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -7,7 +7,7 @@ export class HelperService {
 /// https://perpusstimikclient.vercel.app/
 apiUrl = "https://perpusstimikapi.stimiksepnop.ac.id";
 //apiUrl = "http://localhost:8000";
-//
+
 constructor() { }
 
 }
